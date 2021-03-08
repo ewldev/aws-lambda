55c357c410a9ba2921d429d13d21996a1cc20974
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-03-08 12:38:19 -0500

    modified handler.js to create emoji based on ranking of smart brain login, changed serverless.yml from dev to prod

--- handler.js
+++ handler.js
@@ -1,17 +1,36 @@
 'use strict';
 
+const emojis = [
+  '😄','😃','😀','😊','😉','😍','🔶','🔷', '🚀'
+];
 module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length -1 : rank];
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
     ),
   };
+// module.exports.rank = (event, context, callback) => {
+//   const rank = event.queryStringParameters.rank;
+//   const rankEmoji = emojis[rank >= emojis.length ? emojis.length -1 : rank];
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: rankEmoji,
+//       }),
+//   };
+
+//   callback(null, response);
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
